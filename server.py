6f3a3b335f3b5202483003d4bf6b5358c81db889
Christopher Triolo <ctriolo@princeton.edu>
2012-03-14 05:12:10 -0400

    Restful POST,PUT,DELETE

--- server.py
+++ server.py
@@ -3,10 +3,11 @@
 #
 # server.py
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, abort, redirect, url_for, jsonify
 import pymongo
 from pymongo import Connection
+from pymongo.objectid import ObjectId
+from bson.errors import InvalidId
 
 connection = Connection('localhost', 27017)
 db = connection.getmehere
@@ -17,14 +18,53 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/product/<query>")
-def get_products(query):
-    matches = []
-    for product in db.products.find({"name":{"$regex":"^" + query + ".*"}}):
-        product['_id'] = str(product['_id'])
-        matches.append(product); #{'name': str(product[u'name'])})
-    products = {"count" : len(matches), "rows" : matches}
-    return jsonify(products)
+@app.route("/product", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def product():
+    if request.method == 'GET':
+        if 'id' in request.args:
+            try: product = db.products.find_one({"_id":ObjectId(request.args['id'])})
+            except InvalidId: return jsonify(response=[])
+            if (product is None): return jsonify(response=[])
+            product['_id'] = str(product['_id'])
+            return jsonify(response=[product])
+        elif 'name' in request.args:
+            product = db.products.find_one({"name":request.args['name']})
+            if (product is None): return jsonify(response=[])
+            product['_id'] = str(product['_id'])
+            return jsonify(response=[product])
+        elif 'prefix' in request.args:
+            products = []
+            for product in db.products.find({"name":{"$regex":"^" + request.args['prefix'] + ".*"}}):
+                product['_id'] = str(product['_id'])
+                products.append(product);
+            return jsonify(response=products)
+        else:
+            products = []
+            for product in db.products.find():
+                product['_id'] = str(product['_id'])
+                products.append(product);
+            return jsonify(response=products)
+    elif request.method == 'POST':
+        if (request.json is None): return jsonify(error="POST data is not JSON.")
+        db.products.insert(request.json, manipulate=True)
+        request.json['_id'] = str(request.json['_id'])
+        return jsonify(request.json)
+    elif request.method == 'PUT':
+        if (request.json is None): return jsonify(error="PUT data is not JSON.")
+        request.json['_id'] = ObjectId(request.json['_id'])
+        db.products.save(request.json, manipulate=True)
+        request.json['_id'] = str(request.json['_id'])
+        return jsonify(request.json)
+    elif request.method == 'DELETE':
+        if 'id' in request.args:
+            try: db.products.remove({"_id":ObjectId(request.args['id'])})
+            except InvalidId: return jsonify(response=[])
+            return jsonify(response="ok")
+        elif 'name' in request.args:
+            db.products.remove({"name":request.args['name']})
+            return jsonify(response="ok")
+        return jsonify(error="No parameters.");
+    return jsonify(error="This should never happen.")
 
 if __name__ == "__main__":
     app.run(debug=True)
